3d48ea98058c38e678f0535b2f5c22e06339f651
Nguyen Huu Minh Duy <148361880+duyhxm@users.noreply.github.com>
2024-01-02 22:01:21 +0700

    Update script.js

--- apollon/script.js
+++ apollon/script.js
@@ -349,3 +349,9 @@ function moveSlide(){
         }
     }
 }
+
+//test overflow-x hidden on mobile
+function disableHorizontalScroll() {
+  document.body.style.overflowX = 'hidden';
+}
+window.onload = disableHorizontalScroll;
